8021bd6013d4af44a99bd86ddb894b76b4bf5c68
haimlavi1989 <haimlavi1989@gmail.com>
2024-07-14 22:01:32 +0300

    Refactor code to async pipe

--- src/app/feature/header/header.component.ts
+++ src/app/feature/header/header.component.ts
@@ -1,40 +1,28 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from "./../users/users.service";
-import { Subscription } from 'rxjs/internal/Subscription';
-import { User } from './../users/user.model'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
 
   menuToggle : boolean = false;
-  private usersChangedSubscription: Subscription | undefined;
-  usersLength = 0;
+  usersLength$: Observable<number>;
 
-  constructor(private usersService: UsersService) { }
-
-  ngOnInit(): void {
-      // Keep geting users changes
-      this.usersChangedSubscription = this.usersService.usersChanged.subscribe({
-          next: (response: User[]) => {
-            console.log(response)
-            this.usersLength = response.length;
-          },
-          error: (error) => {
-          }
-      });
+  constructor(private usersService: UsersService) {
+    this.usersLength$ = this.usersService.usersChanged.pipe(
+      map(users => users.length)
+    );
   }
+
+  ngOnInit(): void {}
 
   handleToggle() {
     this.menuToggle = !this.menuToggle;
   }
 
-  ngOnDestroy() {
-    if (this.usersChangedSubscription) {
-      this.usersChangedSubscription.unsubscribe();
-    }
-  }
 }
